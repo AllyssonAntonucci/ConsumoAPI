96916f2b2a34b8c07186ad400eb116a1e888a0af
AllyssonAntonucci <125825975+AllyssonAntonucci@users.noreply.github.com>
2023-11-19 19:05:01 -0300

    Arquivos iniciais adicionados

---
+++ ConsumoAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using modelos;
+using Newtonsoft.Json;
+
+namespace ConsumoAPI
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            HttpClient client = new HttpClient { BaseAddress = new Uri("https://jsonplaceholder.typicode.com/users") };
+            var resposta = await client.GetAsync("users");
+            var conteudo = await resposta.Content.ReadAsStringAsync();
+
+            var users = JsonConvert.DeserializeObject<User[]>(conteudo);
+
+
+            foreach (var item in users)
+            {
+                Console.WriteLine(
+                    $"Nome: {item.Name}, " +
+                    $"Email: {item.Email}, " +
+                    $"Cidade: {item.Address.City}, " +
+                    $"Empresa: {item.Company.Name}."
+                    );
+            }
+
+
+        }
+    }
+}
